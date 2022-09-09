e11441efe5a806fe721c35a9688d810f30f71149
Kotka <kotkadkofficial@gmail.com>
2022-09-09 09:36:35 +0200

    9.36.9.9.22

--- src/components/ColorPreview.jsx
+++ src/components/ColorPreview.jsx
@@ -1,7 +1,10 @@
 function ColorPreview(props) {
     return (
-      <div className={"DivColorPicker Color" + props.number} style={{ backgroundColor: props.color }}>
-        <p className="PColorNumber">{props.number}</p>
+      <div className="tooltip">
+        <div className={"DivColorPicker Color" + props.number} style={{ backgroundColor: props.color }}>
+          <p className="PColorNumber">{props.number}</p>
+        </div>
+        <span class="tooltiptext">{props.color}</span>
       </div>
     );
   }
